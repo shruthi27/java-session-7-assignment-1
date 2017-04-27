3e2bfa2445024c36730d299c126f7aebdd526766
sruthi <shruthi27@users.noreply.github.com>
2017-04-27 17:21:56 +0530

    Add files via upload

---
+++ IntToStringConversion.java
@@ -0,0 +1,17 @@
+package inttostringconversion;
+
+public class IntToStringConversion 
+{
+
+	public static void main(String[] args)
+	{
+		//declare an integer
+		int myinteger = 10;
+		//declare a string.
+		String mystring =" ";//its empty string
+		// we have to convert the integer 10 to string and we have to print it.
+		mystring = Integer.toString(myinteger);
+		System.out.println(mystring);
+		
+	}
+}
